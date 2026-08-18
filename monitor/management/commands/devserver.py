ac866de797ad47c606e1011ce75717b5c815cf93
chrizz <christian.aribal@jujiedso.com>
2026-08-18 14:15:52 +0800

    Prepare project for deployment

---
+++ monitor/management/commands/devserver.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import subprocess
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Start Django development server and PPPoE monitor"
+
+    def handle(self, *args, **options):
+        monitor_process = None
+
+        try:
+            self.stdout.write(
+                self.style.SUCCESS(
+                    "Starting PPPoE monitor..."
+                )
+            )
+
+            monitor_process = subprocess.Popen(
+                [
+                    sys.executable,
+                    "manage.py",
+                    "monitor_pppoe",
+                ]
+            )
+
+            self.stdout.write(
+                self.style.SUCCESS(
+                    "PPPoE monitor started."
+                )
+            )
+
+            self.stdout.write(
+                self.style.SUCCESS(
+                    "Starting Django server..."
+                )
+            )
+
+            os.system(
+                f'"{sys.executable}" manage.py runserver'
+            )
+
+        except KeyboardInterrupt:
+            self.stdout.write(
+                self.style.WARNING(
+                    "\nStopping..."
+                )
+            )
+
+        finally:
+            if monitor_process:
+                self.stdout.write(
+                    self.style.WARNING(
+                        "Stopping PPPoE monitor..."
+                    )
+                )
+
+                monitor_process.terminate()
+
+                try:
+                    monitor_process.wait(timeout=5)
+                except subprocess.TimeoutExpired:
+                    monitor_process.kill()
